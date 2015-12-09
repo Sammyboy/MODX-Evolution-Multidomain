940a09992e3d0691996b145f95db8b403fed327c
Sammyboy <sam@manjaro.org>
2015-12-09 12:45:45 +0100

    Version 1.0

---
+++ assets/lib/DocumentParser_Extended.php
@@ -0,0 +1,145 @@
+<?php
+/**
+ * Multidomain DocumentParser Extended - for MODX Evolution
+ * @category  plugin
+ * @version   1.0
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU Public License (GPL)
+ * @author    Marek Srejma (sam_web@yahoo.de)
+ */
+
+class DocumentParser_Extended extends DocumentParser {
+
+  var $sites = array(); // storage for all sites data
+  var $site;            // current site
+  
+  private function getTVbyID($id, $tv_name) {
+    $tv_arr = $this->getTemplateVar($tv_name,'*',$id);
+    return $tv_arr['value'];
+  }
+  
+  function getSites() {
+    $xml_file = 'assets/files/domains.xml';
+    $err_msg  = '';
+    $xml      = @simplexml_load_file( $this->config['base_path'] . $xml_file, 'SimpleXMLElement', LIBXML_NOCDATA );
+    if ($xml !== false && isset($xml->domain)) {
+      foreach($xml->domain as $d ) {
+        if (isset($d->name)) {
+          $domain = new stdClass();
+          $domain->name              = (string) $d->name;
+          $domain->protocol          = (string) $d->protocol;
+          $domain->site_id           = (int)    $d->site_id;
+          $domain->site_start        = (int)    $d->site_start;
+          $domain->error_page        = (int)    $d->error_page;
+          $domain->offline_page      = (int)    $d->offline_page;
+          $domain->unauthorized_page = (int)    $d->unauthorized_page;
+          $domain->page_not_found    = (int)    $d->page_not_found;
+          $domain->keyword           = (string) $d->keyword;
+          
+          $domain->is_online         = !(bool)  $this->getTVbyID($domain->site_id, 'hidemenu');
+          $domain->alias             = (string) $this->getTVbyID($domain->site_id, 'alias');
+
+          $this->sites['alias'][$domain->alias] = $domain;
+          
+          // set current site
+          if ( $domain->name === $_SERVER['HTTP_HOST'] ) {
+            $this->site = $domain;
+          }
+          unset($domain);
+        } else {
+          $err_msg.= 'ERROR: Failed reading '.$xml_file.'!<br />';
+        }
+      }
+    } else { 
+      $err_msg.= 'ERROR: Failed to open file '.$xml_file.'!<br />';
+    }
+    
+    if ($err_msg !== '') { 
+      print_r($err_msg);
+    }
+    
+    $this->setPlaceholder('domainkey', $this->site->keyword);
+    $this->setPlaceholder('is_online', $this->site->is_online);
+    $this->setPlaceholder('domain', $this->site->name);
+    return;
+  }
+
+  function getDocumentIdentifier($method) {
+    $docIdentifier = (($this->site->is_online || $_SESSION['mgrRole']) ? $this->site->site_start : $this->site->offline_page);
+    switch ($method) {
+      case 'alias' :
+        $docIdentifier= $this->db->escape($_REQUEST['q']);
+        if ($this->config['use_alias_path'] == 1) {
+          // check if domain is marked online:
+          $docIdentifier = $this->site->alias . '/' . 
+            (($this->site->is_online || $_SESSION['mgrRole']) ? $docIdentifier : $this->site->offline_page);
+        }
+        break;
+      case 'id' :
+        if (!is_numeric($_REQUEST['id'])) {
+          $this->sendErrorPage();
+        } else {
+          $docIdentifier = intval($_REQUEST['id']);
+        }
+        break;
+    }
+    return $docIdentifier;
+  }
+
+
+  function sendErrorPage() {
+    $this->invokeEvent('OnPageNotFound');
+    $this->sendForward(
+      $this->site->error_page,
+      'HTTP/1.0 404 Not Found'
+    );
+    exit();
+  }
+
+
+  function sendUnauthorizedPage() {
+    $_REQUEST['refurl'] = $this->documentIdentifier;
+    $this->invokeEvent('OnPageUnauthorized');
+    if ($this->site->unauthorized_page) {
+      $unauthorizedPage= $this->site->unauthorized_page;
+    } elseif ($this->site->error_page) {
+      $unauthorizedPage = $this->site->error_page;
+    } else {
+      $unauthorizedPage= $this->site->site_start;
+    }
+    $this->sendForward($unauthorizedPage, 'HTTP/1.1 401 Unauthorized');
+    exit();
+  }
+
+
+  function makeUrl($id, $alias= '', $args= '', $scheme= '') {
+    $url  = parent::makeUrl($id, $alias, $args, $scheme);
+    if (strpos($url,'/') === 0) { // "/domainalias/folder/document.html"
+      $url = substr($url,1);
+    }
+    $parts = explode('/', $url);
+    if ($parts[0] === $this->site->alias) {
+      unset($parts[0]);
+      $url = $this->config['base_url'] . implode('/',$parts);
+    }
+    if ($other_site = $this->sites['alias'][$parts[0]] ) {
+      $parts[0] = $other_site->protocol . $other_site->name;
+      $url = implode('/',$parts);
+    }
+    return str_replace($this->site->alias."/","",$url);
+  }
+
+
+  function _makeUrl($id) {
+    return $this->makeUrl($id[1]);
+  }
+
+
+  function rewriteUrls($documentSource) {
+    $documentSource = preg_replace_callback(
+      '!\[\~([0-9]+)\~\]!',
+      array($this, '_makeUrl'),
+      $documentSource
+    );
+    return $documentSource; 
+  }
+}
---
+++ assets/lib/multidomain.check_index.php
@@ -0,0 +1,119 @@
+<?php
+/**
+ * Multidomain check index - for MODX Evolution
+ * @category  plugin
+ * @version   1.0
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU Public License (GPL)
+ * @author    Marek Srejma (sam_web@yahoo.de)
+ */
+
+if (! function_exists('ExtendIndexFile')) {
+  function ExtendIndexFile($params) {
+    global $modx;
+    // name of file storing version information
+      $version_file   =   trim($params['version_file']);
+    $indexfile_path  =  trim($params['index_path']);
+    $search4lines  =  $params['search'];
+    $replace2lines  =  $params['replace'];
+
+    $check_version  =   false;
+    $check_sum      =   false;
+    $version_url    =   MODX_BASE_PATH . $version_file;
+    // path of file to check
+    $index_file     =   MODX_BASE_PATH . $indexfile_path . 'index.php';
+
+    $last_version   =   (string)    file($version_url);
+    $akt_vers       =   md5_file($index_file);
+
+    // check version numbers
+    if (!$last_version || ($last_version != $akt_vers)) {
+      $check_version  =   true;
+      $check_sum      =   true;
+    }
+
+    if ($check_version) {
+      // open index.php
+      if ($pt = fopen($index_file, 'r')) {
+        // read index.php file
+        $file_content   =   fread($pt, filesize($index_file));
+        fclose($pt);
+
+        // search and replace
+        $out_content    =   str_replace($search4lines,
+          "\n// ## changed by multidomain-plugin on ".date(DATE_RFC822).": ##\n".
+          $replace2lines."\n// ## end of changes ##\n", $file_content);
+
+        if ($file_content !== $out_content) {
+          // rename old filename
+          if (rename($index_file, $index_file . ".bak")) {
+            // create new index.php
+            if ($pt = fopen($index_file, 'w')) {
+              fwrite($pt, $out_content, strlen($out_content));
+              fclose($pt);
+              $check_sum = true;
+            } else {
+              // error message if file could not be saved
+              print_r("ERROR: Failed to write " . $index_file . "!\n\n");
+              // rerename
+              rename($index_file . ".bak", $index_file);
+            }
+            } else {
+              // error message if original file could not be renamed
+              print_r("ERROR: Faild to update " . $index_file .
+                ". Could not be renamed!\n\n");
+            }
+        } else {
+          $check_version = false;
+        }
+      } else {
+        // error message if index.php could not be opened
+        print_r("ERROR: Failed to read " . $index_file . "!\n\n");
+      }
+    }
+    if ($check_sum) {
+      // create new md5sum-checksum
+      $akt_vers       =   md5_file($index_file);
+      // and save it
+      if ($pt = fopen($version_url, 'w')) {
+        fwrite($pt, $akt_vers, strlen($akt_vers));
+        fclose($pt);
+          // check if there were changes
+        if ($last_version !== $akt_vers) {
+          $check_version = true;
+        }
+      } else {
+        // error message if checksum file could not be written
+        print_r("ERROR: Failed to write " . $version_url . "!\n\n\n");
+      }
+    }
+    return;
+  } // end function
+} // end if
+
+// execute
+ExtendIndexFile(array(
+  'index_path'    => '',
+  'version_file'  => 'assets/files/.index.php.md5',
+  
+  'search'        => "\$modx = new DocumentParser;",
+  
+  'replace'       => "include_once MODX_BASE_PATH.'assets/lib/DocumentParser_Extended.php';\n".
+                     "\$modx = new DocumentParser_Extended();\n".
+                     "\$modx->getSites();"
+));
+
+ExtendIndexFile(array(
+  'index_path'    => 'manager/',
+  'version_file'  => 'assets/files/.manager_index.php.md5',
+  
+  'search'        => "\$modx = new DocumentParser;\n".
+                     "\$modx->loadExtension(\"ManagerAPI\");\n".
+                     "\$modx->getSettings();",
+  
+  'replace'       => "include_once MODX_BASE_PATH.'assets/lib/DocumentParser_Extended.php';\n".
+                     "\$modx  = new DocumentParser_Extended();\n".
+                     "\$modx->loadExtension(\"ManagerAPI\");\n".
+                     "\$modx->getSettings();\n".
+                     "\$modx->getSites();"
+));
+?>
